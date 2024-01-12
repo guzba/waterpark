626b52642f693ed83c1966a2b399b54ec7acc2d8
Thomas T. Jarløv <ThomasTJdev@users.noreply.github.com>
2024-01-12 21:52:31 +0100

    allow PG pool size of 0

--- src/waterpark/postgres.nim
+++ src/waterpark/postgres.nim
@@ -23,8 +23,6 @@ proc newPostgresPool*(
   size: int, connection, user, password, database: string
 ): PostgresPool =
   ## Creates a new thead-safe pool of Postgres database connections.
-  if size <= 0:
-    raise newException(CatchableError, "Invalid pool size")
   result.pool = newPool[DbConn]()
   try:
     for _ in 0 ..< size:
